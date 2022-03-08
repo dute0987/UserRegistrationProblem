7e11351aede5e3240c79aab5322cfb37eb3052c0
dute0987 <dute0987@gmail.com>
2022-03-08 18:47:05 +0530

    Main file for mobile format

---
+++ Patterns.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace RegexDemo
+{
+    public class Patterns
+    {
+        public static string Regex_Pattern = "^[+][0-9]{2}[' '][7-9]{1}[0-9]{5}[0-9]{4}$";
+
+        public bool ValidatePinCode(string PhoneNumber)
+        {
+            return Regex.IsMatch(PhoneNumber, Regex_Pattern);
+        }
+    }
+}
