9e104717bf923a6a79a1b16124fee7e338240826
dute0987 <dute0987@gmail.com>
2022-03-08 20:14:37 +0530

    Main file for EmailId format

---
+++ UC-3-Email.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Detail
+{
+    public class UC_3_Email
+    {
+        public static string EmailPattern = "^[a-z]{3}*[@]*[a-z]{2}*[.]*[co]$";
+
+        public bool ValidateEmail(string EmailID)
+        {
+            return Regex.IsMatch(EmailID, EmailPattern);
+        }
+    }
+}
