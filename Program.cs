14aaabcd18f0b246e4a44e41dc8c7790dba9427c
dute0987 <dute0987@gmail.com>
2022-03-08 19:38:16 +0530

    Program.cs file for Last Name format

--- Program.cs
+++ Program.cs
@@ -6,19 +6,32 @@ namespace Detail
     {
         static void Main(string[]args)
         {
-            UC_1_FirstName UC1 = new UC_1_FirstName();
+            //UC_1_FirstName UC1 = new UC_1_FirstName();
 
-            Console.WriteLine("Enter First name");
+            //Console.WriteLine("Enter First name");
+            //string value = Console.ReadLine();
+            //bool match = UC1.FirstNameSpecified(value);
+
+            //if (match)
+            //{
+                //Console.WriteLine("Patterns Matched");
+
+            //}
+            //else
+                //Console.WriteLine("Patterns Not Matched");
+
+
+            UC_2_LastNamecs UC2 = new UC_2_LastNamecs();
+            Console.WriteLine("Enter Last Name");
             string value = Console.ReadLine();
-            bool match = UC1.FirstNameSpecified(value);
+            bool match = UC2.ValidateLastName(value);
 
             if (match)
             {
-                Console.WriteLine("Patterns Matched");
-
+                Console.WriteLine("Pattern Matched");
             }
             else
-                Console.WriteLine("Patterns Not Matched");
+                Console.WriteLine("Pattern not Matched");
         }
     }
 }
