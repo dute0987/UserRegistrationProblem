9b9943caaba7cae16f4b15f43645ff930ff8b6d5
dute0987 <dute0987@gmail.com>
2022-03-08 19:23:00 +0530

    Main file for First Name format

---
+++ UC-1-FirstName.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Detail
+{
+     public class UC_1_FirstName
+     {
+        public static string FirstName = "^[A-Z]{1}[a-z]{5}$";
+
+        public bool FirstNameSpecified(string firstname)
+        {
+            return Regex.IsMatch(firstname, FirstName);
+        }
+     }
+}
