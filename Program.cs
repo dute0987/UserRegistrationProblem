bae0df0d2438d514f13e589cfaf34039c739fc30
dute0987 <dute0987@gmail.com>
2022-03-08 19:22:19 +0530

    Program.cs file for First Name format

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Detail
+{
+    class Program
+    {
+        static void Main(string[]args)
+        {
+            UC_1_FirstName UC1 = new UC_1_FirstName();
+
+            Console.WriteLine("Enter First name");
+            string value = Console.ReadLine();
+            bool match = UC1.FirstNameSpecified(value);
+
+            if (match)
+            {
+                Console.WriteLine("Patterns Matched");
+
+            }
+            else
+                Console.WriteLine("Patterns Not Matched");
+        }
+    }
+}
