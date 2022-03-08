eb957251ea4b75e2ac895e5b84aa507203822a0d
dute0987 <dute0987@gmail.com>
2022-03-08 20:27:13 +0530

    Main file for Minimum 8 character format

---
+++ UC-5-PasswardRule1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Detail
+{
+    public class UC_5_PasswardRule1
+    {
+        public static string Rule1Pattern = "^[a-z]{8}$";
+
+        public bool ValidateChar(string Passward)
+        {
+            return Regex.IsMatch(Passward, Rule1Pattern);
+        }
+    }
+}
