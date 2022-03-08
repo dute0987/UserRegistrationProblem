ee75f108e5687528bb7a5a33931600791a9b3cba
dute0987 <dute0987@gmail.com>
2022-03-08 19:42:20 +0530

    Main file for Last Name format

---
+++ UC-2-LastNamecs.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Detail
+{
+    public class UC_2_LastNamecs
+    {
+        public static string LastPattern = "^[A-Z]{1}[a-z]{3}$";
+
+        public bool ValidateLastName(string LastName)
+        {
+            return Regex.IsMatch(LastName, LastPattern);
+        }
+    }
+}
