58bd900dfc564e6f9d87015f50c6adab5a0a80ed
dute0987 <dute0987@gmail.com>
2022-03-08 18:46:27 +0530

    Program.cs file for mobile format

---
+++ PatternsProgram.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace RegexDemo
+{
+    class Program
+    {
+        static void Main(string[]args)
+        {
+            Patterns patterns = new Patterns();
+
+            Console.WriteLine("Enter the Phone Number");
+            string value = Console.ReadLine();
+            bool match = patterns.ValidatePinCode(value);
+
+            if (match)
+            {
+                Console.WriteLine("Patterns matched");
+
+            }
+            else
+                Console.WriteLine("Pattens Not Matched");
+        }
+    }
+}
