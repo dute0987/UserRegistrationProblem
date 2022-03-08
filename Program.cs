23b11df0ce1159b27cb59bc2d70cd0e73d39fbef
dute0987 <dute0987@gmail.com>
2022-03-08 20:14:04 +0530

    Program.cs file for EmailId format

--- Program.cs
+++ Program.cs
@@ -21,17 +21,31 @@ namespace Detail
                 //Console.WriteLine("Patterns Not Matched");
 
 
-            UC_2_LastNamecs UC2 = new UC_2_LastNamecs();
-            Console.WriteLine("Enter Last Name");
+            //UC_2_LastNamecs UC2 = new UC_2_LastNamecs();
+            //Console.WriteLine("Enter Last Name");
+            //string value = Console.ReadLine();
+            //bool match = UC2.ValidateLastName(value);
+
+            //if (match)
+            //{
+                //Console.WriteLine("Pattern Matched");
+            //}
+            //else
+                /Console.WriteLine("Pattern not Matched");
+
+            UC_3_Email UC3 = new UC_3_Email();
+            Console.WriteLine("Enter Your Email");
             string value = Console.ReadLine();
-            bool match = UC2.ValidateLastName(value);
+            bool match = UC3.ValidateEmail(value);
 
             if (match)
             {
-                Console.WriteLine("Pattern Matched");
+                Console.WriteLine("Patterns Matched");
+
             }
             else
-                Console.WriteLine("Pattern not Matched");
+                Console.WriteLine("Pattern Not Matched");
+
         }
     }
 }
