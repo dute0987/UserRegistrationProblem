a9532f353017c64b6cb864ebc9554d94d92d139b
dute0987 <dute0987@gmail.com>
2022-03-08 20:38:41 +0530

    Main file for Upper Case Passward

---
+++ UC-6-OneUpperCase.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Detail
+{
+    public class UC_6_OneUpperCase
+    {
+        public static string UpperPattern = "^[A-Z]{1}[a-z]{7}$";
+
+        public bool ValidateUpper(string Passward)
+        {
+            return Regex.IsMatch(Passward, UpperPattern);
+        }
+
+    }
+}
