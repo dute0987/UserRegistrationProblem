f46135eef43a21abc2b7ef3f46f7077ec9b9bce5
dute0987 <dute0987@gmail.com>
2022-03-08 20:51:11 +0530

    Main file for Atleast One Numeric Number Passward

---
+++ UC-7-OneNumericNumber.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Detail
+{
+    public class UC_7_OneNumericNumber
+    {
+        public static string NumericPattern = "^[A-Z]{1}[a-z]{1}[0-9]{6}$";
+
+        public bool ValidateNumeric(string number)
+        {
+            return Regex.IsMatch(number, NumericPattern);
+        }
+    }
+}
